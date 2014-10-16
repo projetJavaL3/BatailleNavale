dd4da55db87e4fa2bb1cbb1b6c4db1de841947b7
theo91540 <theochelim@hotmail.fr>
2014-10-16 09:34:45 +0200

    L'info sur commit

--- Bataille.java
+++ Bataille.java
@@ -1,4 +1,5 @@
 public class Bataille extends B
 {
-  //   Je pense que je ne suis pas le seul a trouver que ce fichier est legerement vide ! 
+    fuiegzyfgezygf yuezgfy gfyez
+    //   Je pense que je ne suis pas le seul a trouver que ce fichier est legerement vide ! 
 }
