215de4075f6fcaf81ae5d6726f46b903d27f6c09
Maxime91860 <jaco322@hotmail.fr>
2014-10-16 11:48:29 +0200

    Creation de la classe Position

---
+++ src/BatailleNavale/Model/Position.java
@@ -0,0 +1,133 @@
+/** 
+ * 16/10/2014
+ * @author Maxime Kermarquer - Brady Abderemane
+ * @version 1.0.0
+ */
+
+
+public class Position {
+	/**
+	 * Coordonnee X
+	 */
+	private int coord_X;
+	/**
+	 * Coordonnee Y
+	 */
+	private int coord_Y;
+	
+	/**
+	 * Construit une <code>Position</code> à partir de deux entiers x,y.
+	 * @param x
+	 * @param y
+	 * @throws IllegalArgumentException
+	 */
+	public Position (int x, int y) throws IllegalArgumentException {
+		if(x<0 || y<0){
+			throw new IllegalArgumentException();
+		}
+		coord_X = x;
+		coord_Y = y;
+	}
+	
+	/**
+	 * Construit une <code>Position</code> à partir d'une autre.
+	 * @param p
+	 * @throws NullPointerException
+	 */
+	public Position (Position p) throws NullPointerException {
+		if( p == null ){
+			throw new NullPointerException();
+		}
+		coord_X = p.coord_X;
+		coord_Y = p.coord_Y;
+	}
+
+	/**
+	 * Accesseur de <code>coord_X</code>.
+	 * @return coord_X
+	 */
+	public int getCoord_X() {
+		return coord_X;
+	}
+
+	/**
+	 * Modifieur de <code>coord_X</code>.
+	 * @param coord_X
+	 */
+	public void setCoord_X(int coord_X) {
+		this.coord_X = coord_X;
+	}
+
+	/**
+	 * Accesseur de <code>coord_Y</code>.
+	 * @return coord_Y
+	 */
+	public int getCoord_Y() {
+		return coord_Y;
+	}
+
+	/**
+	 * Modifieur de <code>coord_Y</code>.
+	 * @param coord_Y
+	 */
+	public void setCoord_Y(int coord_Y) {
+		this.coord_Y = coord_Y;
+	}
+	
+	/**
+	 * Modifieur de <code>coord_X</code> et <code>coord_Y</code>.
+	 * @param coord_X
+	 * @param coord_Y
+	 */
+	public void setCoord_XY (int coord_X, int coord_Y){
+		setCoord_X(coord_X);
+		setCoord_Y(coord_Y);
+	}
+	
+	/**
+	 * Calcule la distance approchee entre deux <code>Position</code>.
+	 * @param p
+	 * @return
+	 * @throws NullPointerException
+	 */
+	public int distance (Position p) throws NullPointerException{
+		if(p==null){
+			throw new NullPointerException();
+		}
+		//Conversion un peu degueulasse
+		return (int)Math.sqrt( (p.coord_X - coord_X)*(p.coord_X - coord_X) + (p.coord_Y - coord_Y)*(p.coord_Y - coord_Y));
+	}
+	
+	
+	/*
+	 * Calcul la distance exacte 
+	 * 
+	 * 
+	public double distance2 (Position p) throws NullPointerException{
+		if(p==null){
+			throw new NullPointerException();
+		}
+		//Conversion un peu degueulasse
+		return Math.sqrt( (p.coord_X - coord_X)*(p.coord_X - coord_X) + (p.coord_Y - coord_Y)*(p.coord_Y - coord_Y));
+	}
+	*/
+	
+	public String toString(){
+		return "("+coord_X+","+coord_Y+")";
+	}
+	
+	public boolean equals(Object o){
+		if(o == null || !(o instanceof Position)){
+			return false;
+		}
+		Position tmp = (Position)o;
+		if(tmp.coord_X != coord_X){
+			return false;
+		}
+		if(tmp.coord_Y != coord_Y){
+			return false;
+		}	
+		return true;
+	}
+	
+}
