f2a1ccb3a897ef083a5fcde1b2ff2efee5485b1d
Brady91700 <abderemane_brady@live.fr>
2014-10-23 17:14:01 +0200

    Modification de la classe partie(test)

--- src/BatailleNavale/Model/Partie.java
+++ src/BatailleNavale/Model/Partie.java
@@ -12,6 +12,33 @@ public class Partie {
 	 */
 	protected Type_partie partie;
 	
+	/**
+	 * Constructeur de la Partie 
+	 */
+	public Partie(){
+		
+		this.partie = Partie.ARCADE;
+	    this.joueur = new ArrayList<Joueur>;
+	}
+	
+	/**
+	 * Retroune le type de partie
+	 */
+	public Type_Partie getTypePartie()
+	{
+		return this.partie;
+	}
+	/**
+	 * Modifie le type de Partie
+	 */
+	public void setTypePartie(Type_partie new_partie)
+	{
+		this.partie = new_partie;
+	}
+	/**
+	 * Pour savoir si la Partie est terminé.
+	 * @return true
+	 */
 	private boolean estTermine(){
 		return true;
 	}
