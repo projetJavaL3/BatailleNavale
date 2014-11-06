e7a0a9b8866f2dda627c972509bb1099334d1e61
Maxime91860 <jaco322@hotmail.fr>
2014-11-06 12:00:00 +0100

    Test dans la classe Modele

--- src/BatailleNavale/Model/Modele.java
+++ src/BatailleNavale/Model/Modele.java
@@ -16,6 +16,7 @@ public class Modele extends Observable
 	 */
 	private Partie partie;
 	private Options options;
+	private Position p;
 
 	/**
 	 * Constructeur du Modele
@@ -78,16 +79,38 @@ public class Modele extends Observable
 		h2.ajouterBateau(new Cuirasse());
 		h2.ajouterBateau(new SousMarin());
 		h2.ajouterBateau(new Zodiac());
+		
+		//ajout de Porte-Avion
+		h2.ajouterBateau(new PorteAvion());
+		
 
 		Bateau[] bateauxH2 = h2.getBateaux();
-
-		h2.placerBateau(bateauxH2[0], new Placement(new Position(4,4), true));
-		h2.placerBateau(bateauxH2[1], new Placement(new Position(14,6), false));
-		h2.placerBateau(bateauxH2[2], new Placement(new Position(1,15), true));
-
+		
+		Position p0 = new Position(6,4);
+		Position p1 = new Position(14,4);
+		Position p2 = new Position(1,15);
+		Position p3 = new Position(6,8);
+		
+		h2.placerBateau(bateauxH2[0], new Placement(p0, true));
+		h2.placerBateau(bateauxH2[1], new Placement(p1, false));
+		h2.placerBateau(bateauxH2[2], new Placement(p2, true));
+		
+		h2.placerBateau(bateauxH2[3], new Placement(p3, true));
+		
+		
+		
 		Bloc[] blocs = h2.getChampDeBataille().getEmplacements();
 
+		/**
+		 * Bloc Toucher(temporaire)
+		 */
 		blocs[1].setEtatBloc(Etat_bloc.TOUCHE);
+		bateauxH2[0].retirerPointDeVie();
+		blocs[2].setEtatBloc(Etat_bloc.TOUCHE);
+		bateauxH2[0].retirerPointDeVie();
+		blocs[5].setEtatBloc(Etat_bloc.TOUCHE);
+		bateauxH2[1].retirerPointDeVie();
+		
 
 		for(int i=0; i<blocs.length; i++)
 		{
@@ -95,5 +118,35 @@ public class Modele extends Observable
 		}
 
 		System.out.println(h2.getChampDeBataille());
+		
+		System.out.println("Cuirasse : "+bateauxH2[0].getTaille());
+		System.out.println("Point de Vie : "+bateauxH2[0].getPointDeVie());
+		System.out.println("\nSous marin : "+bateauxH2[1].getTaille());
+		System.out.println("Point de Vie : "+bateauxH2[1].getPointDeVie());
+		System.out.println("\nZodiac : "+bateauxH2[2].getTaille());
+		System.out.println("Point de Vie : "+bateauxH2[2].getPointDeVie());
+		
+		System.out.println("\nPosition Cuirasse : "+p0);
+		System.out.println("Position Cuirasse : "+p1);
+		System.out.println("\nDistance entre Cuirasse et Sous-Marin : "+p0.distance(p1)+" bloc (ou km pour les intimes).");
+		
+		System.out.println("\nDistance entre Cuirasse et Sous-Marin : "+p0.distance(p1)+" bloc (ou km pour les intimes).");
+		
+		int j;
+		for(j=0;j<=blocs.length;j++)
+		{
+			if(j<3)
+				System.out.println("Cuirasse : "+blocs[j].getEtatBloc());
+			
+			else if(j<7)
+				System.out.println("Sous-Marin : "+blocs[j].getEtatBloc());
+			
+			else if(j<9)
+				System.out.println("Zodiac :"+blocs[j].getEtatBloc());
+			
+			else if(j<13)
+				System.out.println("Porte-Avion :"+blocs[j].getEtatBloc());
+		}
+		
 	}
 }
