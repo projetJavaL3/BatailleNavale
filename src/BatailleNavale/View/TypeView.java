672ff6d2bbd0e8b4570d8e3f8616f91dfbede826
Yanis91390 <yab_91@hotmail.fr>
2014-11-23 17:08:50 +0100

    Positionnement des différents composants de la Classe TypeView

--- src/BatailleNavale/View/TypeView.java
+++ src/BatailleNavale/View/TypeView.java
@@ -12,11 +12,12 @@ import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 
 import java.awt.Dimension;
 
 public class TypeView extends AbstractView
-{
+{		
 	public TypeView(MainView view, Modele modele)
 	{	
 		super(view, modele);
@@ -25,31 +26,40 @@ public class TypeView extends AbstractView
 	
 	public void initPanel()
 	{	
-		JButton bouton1 = new JButton("Classique");
-		JButton bouton2 = new JButton("Radar");
-		JButton bouton3 = new JButton("Artillerie");
-		JButton bouton4 = new JButton("Alerte");
+		JButton b_classique = new JButton("Classique");
+		JButton b_radar = new JButton("Radar");
+		JButton b_artillerie = new JButton("Artillerie");
+		JButton b_alerte = new JButton("Alerte");
 
-		this.panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		//On initialise notre Layout à null pour pouvoir appliquer la méthode setBounds()
+		this.panel.setLayout(null);
 		
-		this.panel.add(bouton1);
-		this.panel.add(bouton2);
-		this.panel.add(bouton3);
-		this.panel.add(bouton4);
+		//setBounds(Coordonée x, Coordonee y, Largeur du composant, Hauteur du composant)
+		b_classique.setBounds(10, 100, 160, 50);
+		b_radar.setBounds(175, 100, 170, 50);
+		b_artillerie.setBounds(350, 100, 170, 50);
+		b_alerte.setBounds(525, 100, 165, 50);
+		
+		this.panel.add(b_classique);
+		this.panel.add(b_radar);
+		this.panel.add(b_artillerie);
+		this.panel.add(b_alerte);
 		
 		JComboBox<String> combobox = new JComboBox<String>();
 
-		combobox.addItem("Selectioner le nombre de Joueurs");
+		combobox.addItem("Selectionner le nombre de Joueurs");
 		for(int i = 0 ; i<=10 ; i++)
 		{
 			combobox.addItem("" + i);
-		}		
+		}
+	
+		combobox.setBounds(5,240,690,30);		
 		
-		JButton bouton_suivant = new JButton("Suivant");
-		this.panel.add(bouton_suivant, BorderLayout.SOUTH);
+		JButton b_suivant = new JButton("Suivant");
+		b_suivant.setBounds(300 , 470 , 100 , 40);
 		
+		this.panel.add(b_suivant);
 		this.panel.add(combobox);
-		
 
 	}
 
