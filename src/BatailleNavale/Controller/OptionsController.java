8065ad31c47bcc8db22b6d580558f0ebbc38ddda
Yanis91390 <yab_91@hotmail.fr>
2014-12-05 20:02:50 +0100

    Mise à jours du Controleur OptionsController

--- src/BatailleNavale/Controller/OptionsController.java
+++ src/BatailleNavale/Controller/OptionsController.java
@@ -4,9 +4,15 @@ package BatailleNavale.Controller;
 import BatailleNavale.Model.*;
 import BatailleNavale.View.*;
 
-import java.awt.event.*;
+import java.awt.Checkbox;
 
-public class OptionsController extends AbstractController implements ActionListener
+import java.awt.event.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class OptionsController extends AbstractController implements ActionListener,ItemListener
 {
 	private OptionsView view;
 
@@ -26,6 +32,50 @@ public class OptionsController extends AbstractController implements ActionListe
     		{
     			fenetre.changerVue(new AccueilView(fenetre, modele));
     		}
-    	
 	}  
+	
+	public void itemStateChanged(ItemEvent event)
+	{	
+		/*if (view.getBoutonPortesAvions().getState() == true) 
+		{	
+          		System.out.println("Portes Avions selectionner " );
+       		}*/	
+		int state = event.getStateChange();
+		if (event.getItemSelectable() == view.getBoutonPortesAvions() && state == ItemEvent.SELECTED)
+		{
+				System.out.println("Portes Avions selectionné " );
+		}
+		else if(event.getItemSelectable() == view.getBoutonSousMarin() && state == ItemEvent.SELECTED)
+		{
+				System.out.println("Sous Marins selectionné " );
+		}
+		else if(event.getItemSelectable() == view.getBoutonCuirassée() && state == ItemEvent.SELECTED)
+		{
+				System.out.println("Cuirassé furtif selectionné " );
+		}
+		else if(event.getItemSelectable() == view.getBoutonCuirassée2() && state == ItemEvent.SELECTED)
+		{
+				System.out.println("Cuirassé furtif 2 selectionné " );
+		}
+		else if(event.getItemSelectable() == view.getBoutonZodiac() && state == ItemEvent.SELECTED)
+		{
+				System.out.println("Zodiac selectionné " );
+		}
+		else if(event.getItemSelectable() == view.getBoutonFacile() && state == ItemEvent.SELECTED)
+		{
+				fenetre.afficherMessage("Ca va être easy !" , "Mode Facile" );
+		}
+		else if(event.getItemSelectable() == view.getBoutonMoyen() && state == ItemEvent.SELECTED)
+		{
+				fenetre.afficherMessage("Tu devrais t'en sortir ! " , "Mode Moyen");
+		}
+		else if(event.getItemSelectable() == view.getBoutonDifficile() && state == ItemEvent.SELECTED)
+		{
+				fenetre.afficherMessage("Ca va être chaud !","Mode Difficile" );
+		}
+      }	
+		
+	public void mouseReleased(MouseEvent event){}  
+    	public void mousePressed(MouseEvent event){}
+   	public void mouseClicked(MouseEvent event){}
 }
